70f87c646da8749764e254ba6d0fa430aa79ede0
Edge F <edwidgefabre@gmail.com>
2021-01-18 10:02:20 -0500

    Adjusted Campaign macros to not rely on camscroll#

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,31 +1,56 @@
+require('dotenv').config()
 const fs = require('fs');
 const editJsonFile = require("edit-json-file");
 const path = require('path');
-const lineReplace = require('line-replace')
-require('dotenv').config()
+const date = require('date-and-time');
+var AdmZip = require('adm-zip');
 
-const newTime = "20210106T170304"
+const now = new Date();
+const newTime = date.format(now, 'YYYYMMDDTHHmmss')
 const macrosSubdir = process.env.MACROS_DIR
-// const regex = /([A-Z0-9]){15}/g
-console.log(macrosSubdir);
-//requiring path and fs modules
+const bluestacksMacrosSubdir = process.env.BLUESTACKS_MACROS_DIR
 
-//passsing macrosSubdir and callback function
-fs.readdir(macrosSubdir, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        let fileJSON = editJsonFile(path.join(macrosSubdir,file));
-        fileJSON.unset("SourceRecordings")
-        fileJSON.set("TimeCreated", newTime)
-        fileJSON.save();
-        fs.rename(path.join(macrosSubdir,file), path.join(macrosSubdir,file.toLowerCase()), function(err) {
-            if ( err ) console.log('ERROR: ' + err);
-        });
-        console.log(file.toLowerCase());
+function clearDir(opts) {
+
+    fs.readdir(opts.directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(opts.directory, file), err => {
+                if (err) throw err;
+            });
+        }
     });
-});
+}
+
+function makeBackup() {
+    var zip = new AdmZip();
+    zip.addLocalFolder(bluestacksMacrosSubdir)
+    zip.writeZip(path.join(__dirname, 'backups', `bsmacros_${newTime}.zip`));
+}
+
+function cleanMacros() {
+    fs.readdir(bluestacksMacrosSubdir, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            let fileJSON = editJsonFile(path.join(bluestacksMacrosSubdir, file));
+            fileJSON.unset("SourceRecordings")
+            fileJSON.set("TimeCreated", newTime)
+            fileJSON.save();
+            fs.rename(path.join(bluestacksMacrosSubdir, file), path.join(bluestacksMacrosSubdir, file.toLowerCase()), function (err) {
+                if (err) console.log('ERROR: ' + err);
+                fs.copyFileSync(path.join(bluestacksMacrosSubdir, file.toLowerCase()), path.join(macrosSubdir, file.toLowerCase()))
+            });
+
+        });
+    });
+    console.log("Finished cleaning macros");
+}
+clearDir({ directory: macrosSubdir })
+makeBackup()
+cleanMacros()
